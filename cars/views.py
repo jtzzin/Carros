2546894ff8ff5ca035bfd45215c43380c8365a1f
Juan Thales <juanthalesap@gmail.com>
2025-12-07 18:58:36 -0300

    variavel cars na view

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,11 @@
 # importando a lib render, que renderiza no navegador o html dos templates
 from django.shortcuts import render
+from cars.models import Car
 
 # respota do get na url
 # busca o html de render
 def cars_view(request):
+     cars = Car.objects.all() # busca todos os modelos de carros - querySet
+    
      # request da url, e o caminho do nosso html
-     return render(request, 'cars.html', {'cars': {'model': 'santana 1.8'} } )
+     return render(request, 'cars.html', {'cars': cars } ) # passando os modelos de carros na pagina web
