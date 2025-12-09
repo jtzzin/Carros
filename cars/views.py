c44500eb77c5546136a04c2a668ccb00cac63499
Juan Thales <juanthalesap@gmail.com>
2025-12-08 22:03:28 -0300

    alteracao nas buscas de carros por filter

--- cars/views.py
+++ cars/views.py
@@ -5,7 +5,10 @@ from cars.models import Car
 # respota do get na url
 # busca o html de render
 def cars_view(request):
-     cars = Car.objects.all() # busca todos os modelos de carros - querySet
+     print(request.GET.get('search')) # pega a requisicao get do usuario n0 valor -- /search
+
+     #aqui, o "ID" representa o id da marca, se quiser buscar por nome, deve usar -> brand__name <- # busca todos os modelos de carros - querySet
+     cars = Car.objects.all() #ID por ordem de criaçao (id=1) ou name 
     
      # request da url, e o caminho do nosso html
      return render(request, 'cars.html', {'cars': cars } ) # passando os modelos de carros na pagina web
