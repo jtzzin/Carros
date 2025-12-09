ba96686c099fb819ba44fc868012b14e9fa3eb8a
Juan Thales <juanthalesap@gmail.com>
2025-12-09 18:11:40 -0300

    consultas com ICONTAINS e ORDER_BY

--- cars/views.py
+++ cars/views.py
@@ -5,11 +5,15 @@ from cars.models import Car
 # respota do get na url
 # busca o html de render
 def cars_view(request):
+     # se bater na rota /cars padrao
+     cars = Car.objects.all().order_by("model") # retorna todos os carros e retorna por ordem alfabetica. se eu colcoar -model, ele faz do Z ao A
+
      #capturando a requisicao via url /search=
      search = request.GET.get("search")
-     if search:
-          #aqui, o "ID" representa o id da marca, se quiser buscar por nome, deve usar -> brand__name <- # busca todos os modelos de carros - querySet
-          cars = Car.objects.filter(model__contains=search) #retornando o model do carro
+     
+     if search: # verificando se esta com o campo
+          #aqui, poderia ser "ID" representando o id da marca, se quiser buscar por nome, deve usar -> brand__name <- # busca todos os modelos de carros - querySet
+          cars = Car.objects.filter(model__icontains=search) #retornando o model do carro, "icontains" ignora regra de minusculas e maiusculas. "order_by" ordena por campo do banco
 
      # request da url, e o caminho do nosso html
      return render(
